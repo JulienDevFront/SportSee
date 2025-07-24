817129affc5024eb68aa27fcd8cff739f4fefbbd
Julien Genty <mp.dev.mg@gmail.com>
2025-07-24 23:52:27 +0200

    The frontend is INIT with a minimal version

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './assets/styles/index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <h1>Hello world 👋</h1>
+  </StrictMode>,
+)
